434de65b30b927009aea872d1cdb1855575b7638
=AZIZI Anis <=anis.azizi@etu.univ-lyon1.fr>
2024-04-27 17:06:23 +0200

    ajouts des arguments

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # Importing your classes and functions
 import aspic_generator as ag
 
 # Function to display rules in a table
 def display_rules(rules):
-    st.header("Rules")
     if rules:
         for rule in rules:
             st.write(rule)  # Increase font size for better readability 
 
+def show_all_arguments(arguments):
+    for argument in arguments:
+        st.write(argument)
+
 # Main function to run the Streamlit app
 def main(rules):
-    strict_rules = ag.strict_rules
-    defeasible_rules = ag.defeasible_rules
     st.write('''
              # Welcome to the Argumentation Framework Generator!
                 ## This is a tool to generate argumentation frameworks using the aspicGenerator library. 
@@ -24,6 +26,8 @@ def main(rules):
     st.sidebar.header("Add Rule")
 
     # Add form elements to the sidebar
+    st.sidebar.write("Enter the premises and conclusion of the rule.")
+    st.sidebar.write("Separate premises with commas and a negative premise is '-'premise.")
     premises = st.sidebar.text_input("Premises", "")
     premises = [literal.strip() for literal in premises.split(",")]
     premises = [ag.Literal(literal[1:], is_negative=True) if literal.startswith("~") else ag.Literal(literal) for literal in premises]
@@ -52,11 +56,25 @@ def main(rules):
         display_rules(rules)
     
     # Button to execute create_contrapositions
-    if st.button("Create Contrapositions"):
-        contraposition_rules = ag.create_contrapositions(strict_rules,len(rules))
+    if st.sidebar.button("Create Contrapositions"):
+        # Generate contraposition rules
+        strict_rules = ag.strict_rules
+        contraposition_rules = ag.create_contrapositions(strict_rules, len(rules))
+        
+        # Create ArgumentationFramework object
+        af = ag.ArgumentationFramework(rules + contraposition_rules)
         st.header("Contraposition Rules")
-        rules.extend(contraposition_rules)
-        display_rules(rules)
+        display_rules(contraposition_rules)
+    
+    # Button to show arguments
+    if st.sidebar.button("Show Arguments"):
+        # Generate arguments
+        strict_rules = ag.strict_rules
+        contraposition_rules = ag.create_contrapositions(strict_rules, len(rules))
+        af = ag.ArgumentationFramework(rules + contraposition_rules)
+        arguments = af.get_arguments()
+        st.header("Arguments")
+        show_all_arguments(arguments)
 
 if __name__ == "__main__":
     # Initialize an empty list to store rules
